d285fba6eb078b3398e1cbc92108ba54a3f3318c
Matheus Carvalho <matheus10277@gmail.com>
2021-04-11 14:18:37 -0300

    Added token property on store

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,15 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import Feed from "../views/Feed.vue";
 import Login from "../views/Login.vue";
 
-const routes: Array<RouteRecordRaw> = [  
+const routes: Array<RouteRecordRaw> = [
   {
-    path: "/login",    
+    path: "/login",
     component: Login,
+  },
+  {
+    path: "/feed",
+    component: Feed,
   },
 ];
 
